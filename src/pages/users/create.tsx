8681177cc7180dfc8460761f01e810fe661993df
Cacio de Castro <cacio.castro@gmail.com>
2022-08-08 15:40:13 -0300

    criação das telas da aplicação

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,62 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  VStack,
+} from "@chakra-ui/react";
+
+import { Input } from "../../components/Form/input";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function CreateUser() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex
+        width="100%"
+        marginY="6"
+        marginX="auto"
+        paddingX="6"
+        maxWidth={1480}
+      >
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} backgroundColor="gray.800" padding="8">
+          <Heading fontSize="large" fontWeight="normal">
+            Criar usuário
+          </Heading>
+
+          <Divider marginY="6" borderColor="gray.700" />
+
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
+              <Input name="name" label="Nome completo" />
+              <Input name="email" type="email" label="E-mail" />
+            </SimpleGrid>
+
+            <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
+              <Input name="password" type="password" label="Senha" />
+              <Input
+                name="password_confirmation"
+                type="password"
+                label="Confirmação da senha"
+              />
+            </SimpleGrid>
+          </VStack>
+          <Flex marginTop="8" justifyContent="flex-end">
+            <HStack spacing="4">
+              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Button colorScheme="pink">Salvar</Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
---
+++ src/pages/users/index.tsx
@@ -0,0 +1,102 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
+import { Header } from "../../components/Header";
+import Pagination from "../../components/Pagination";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex
+        width="100%"
+        marginY="6"
+        marginX="auto"
+        paddingX="6"
+        maxWidth={1480}
+      >
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} backgroundColor="gray.800" padding="8">
+          <Flex
+            marginBottom="8"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Heading size="lg" fontWeight="normal">
+              Listagem de usuários
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th paddingX="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th width="8" />
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td paddingX="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Cacio de Castro</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      cacio.castro@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Text>08 de agosto, 2022</Text>
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
